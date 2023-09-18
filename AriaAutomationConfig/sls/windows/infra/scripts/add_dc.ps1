bad140c2e011d5e1e5eb95bf4f1c2bf5284abdef
Christian Mohn <christian@drible.net>
2023-09-18 13:51:51 +0200

    Update add_dc.ps1

--- AriaAutomationConfig/sls/windows/infra/scripts/add_dc.ps1
+++ AriaAutomationConfig/sls/windows/infra/scripts/add_dc.ps1
@@ -2,17 +2,15 @@
 # Version 0.1
 #
 # Description: Promote a new server to a domain controller
-# Required credentials — Need to find a script that works.
+
 # Declare variables
-
-Set-Content -Path 'C:\saltstack.txt' -Value 'add_dc.ps1 Status is; I ran!'
-
 $DatabasePath = "c:windowsNTDS"
+$DomainMode = "WinThreshold"
 $DomainName = "area51.internal"
-$DomaninNetBIOSName = "AREA51"
+$DomainNetBIOSName = "AREA51"
+$ForestMode = "WinThreshold"
 $LogPath = "c:windowsNTDS"
 $SysVolPath = "c:windowsSYSVOL"
 $Password = (ConvertTo-SecureString -String "Pass1w0rd" -AsPlainText -Force)
-
-# Add New DC
-Install-ADDSDomainController -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainName $DomainName -SafeModeAdministratorPassword $Password -LogPath $LogPath -InstallDns:$true -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true
+# Create New AD Forest
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainMode $DomainMode -DomainName $DomainName -SafeModeAdministratorPassword $Password -DomainNetbiosName $DomainNetBIOSName -ForestMode $ForestMode -InstallDns:$true -LogPath $LogPath -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true
